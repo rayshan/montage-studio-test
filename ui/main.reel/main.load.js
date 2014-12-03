cff022b36a4c4b524c4b24b797043f9a2c714d59
Ray Shan <noreply>
2014-12-03 06:58:03 +0000

    Publish build

---
+++ ui/main.reel/main.load.js
@@ -0,0 +1,1 @@
+montageDefine("b61060d","ui/main.reel/main",{dependencies:["montage/ui/component"],factory:function(e,t){var n=e("montage/ui/component").Component;t.Main=n.specialize({constructor:{value:function(){this.super()}}})}});
---
+++ ui/task.reel/task.load.js
@@ -0,0 +1,1 @@
+montageDefine("b61060d","ui/task.reel/task",{dependencies:["montage/ui/component"],factory:function(e,t){var n=e("montage/ui/component").Component;t.Task=n.specialize({constructor:{value:function(){this.super()}}})}});
